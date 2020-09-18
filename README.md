4f5235f6d936668cec2707b4f269f609ee85ec81
Dellaire <henry.borasch@freenet.de>
2020-09-19 00:08:26 +0200

    added endpoint for downloading result as file

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 The _Parallel Url Reader_ performs a `GET` request to a given URL and parses the retrieved content for additional URLs. The new found URLs will also be visited. This continues, until the specified depth is reached. Every found URL and the number of its occurrences is collected and aggregated to a map, which is finally send as response.
 
-## Retrieving a JSON as response
+## Retrieving plain JSON
 
-A call could look like this:
 ```
 curl "http://localhost:8080/urls?rootUrl=https%3A%2F%2Fde.wikipedia.org%2Fwiki%2FWikipedia:Hauptseite&depth=1"
 ```
 
-## Retrieving a JSON as text file
+## Retrieving a JSON file
 
+```
+curl -H "Accept: application/octet-stream" "http://localhost:8080/urls?rootUrl=https%3A%2F%2Fde.wikipedia.org%2Fwiki%2FWikipedia:Hauptseite&depth=1" --output urls.json
+```
--- src/main/java/de/education/parallelurlreader/UrlController.java
+++ src/main/java/de/education/parallelurlreader/UrlController.java
@@ -2,15 +2,21 @@ package de.education.parallelurlreader;
 
 import java.util.concurrent.ExecutionException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RestController
 @RequestMapping("/urls")
 public class UrlController {
+
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public UrlCountSummary getUrlsAsJson(@RequestParam String rootUrl, @RequestParam Integer depth)
@@ -19,5 +25,13 @@ public class UrlController {
 		NodeProcessor nodeProcessor = new NodeProcessor("root", rootUrl, 0, depth);
 
 		return new UrlCountSummary(nodeProcessor);
+	}
+
+	@GetMapping(produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public byte[] getUrlsAsFile(@RequestParam String rootUrl, @RequestParam Integer depth) throws Exception {
+
+		UrlCountSummary summary = new UrlCountSummary(new NodeProcessor("root", rootUrl, 0, depth));
+
+		return this.objectMapper.writeValueAsBytes(summary);
 	}
 }
